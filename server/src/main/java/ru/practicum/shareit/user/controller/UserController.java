c13e444bbc4908e1a04b8d48f037fa90d5ba4ebb
nixanok <mrfreemen555@gmail.com>
2023-11-08 23:10:37 +0300

    чекстайл))

--- server/src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.model.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
